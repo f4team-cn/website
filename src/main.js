65f7fa74733131e6fa04e594f25c0353c21510bd
Yingyya <yyaiwy666@163.com>
2025-08-12 15:08:46 +0800

    publish.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import {createApp} from 'vue';
 import './style.css';
 import App from './App.vue';
+import {inject} from '@vercel/analytics';
 
+inject({
+    debug: process.env.NODE_ENV === 'development',
+});
 createApp(App).mount('#app');
